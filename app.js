09491148eed878de6c72b748fedfc7b09c1d7ebe
EDMerstratec <erme0001@edu.mercantec.dk>
2023-11-22 13:19:34 +0100

    getText() calls url

--- app.js
+++ app.js
@@ -38,12 +38,15 @@ inputElements.forEach((input) => {
 });
 
 
-getText("/Assets/valid-wordle-words.txt");
+getText("https://gist.githubusercontent.com/dracos/dd0668f281e685bad51479e5acaadb93/raw/6bfa15d263d6d5b63840a8e5b64e04b382fdb079/valid-wordle-words.txt");
 
 async function getText(file) {
   let myObject = await fetch(file);
   let myText = await myObject.text();
   myTest = myText.split('\n');
+  if (myTest[myTest.length - 1] === '') { //For some reason, last line of pulled file has a newline after last word. Pop it off to remove blank at last index
+    myTest.pop();
+  }
   correct = myTest[Math.floor(Math.random() * myTest.length)];
 
 }
